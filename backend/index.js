5362ee31f0792c4e65f831239803cab076b6829c
Punnapareddy Bhaskar Rao <bhasaprp2020@gmail.com>
2024-12-06 13:41:08 +0530

    MongoDB Connected

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,12 @@
-const express=require("express")
-const app=express()
-app.listen(3000,()=>{
-    console.log("server is running on port 3000")
-})
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const app = express();
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch((err) => console.log(err));
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
