cafa54699e8c9750ee3d7b69f752e0dc63a22c0f
Punnapareddy Bhaskar Rao <bhasaprp2020@gmail.com>
2024-12-08 22:03:47 +0530

    Make the dash board private

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,32 @@
-import { BrowserRouter,Route,Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Signup from "./pages/Signup"
-import Signin from "./pages/Signin"
-import Dashboard from "./pages/Dashboard"
-import Projects from "./pages/Projects"
-import Header from "./components/Header"
-import FooterComp from "./components/Footer"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Header from "./components/Header";
+import FooterComp from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
-   <BrowserRouter>
-   <Header/>
-    <Routes>
-      <Route path="/" element={<Home/>}></Route>
-      <Route path="/about" element={<About/>}></Route>
-      <Route path="/signin" element={<Signin/>}></Route>
-      <Route path="/signup" element={<Signup/>}></Route>
-      <Route path="/dashboard" element={<Dashboard/>}></Route>
-      <Route path="/projects" element={<Projects/>}></Route>
-    </Routes>
-    <FooterComp/>
-   </BrowserRouter>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/signin" element={<Signin />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+        </Route>
+
+        <Route path="/projects" element={<Projects />}></Route>
+      </Routes>
+      <FooterComp />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
