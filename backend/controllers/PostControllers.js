cc9a669d1bd7e79cecc596bb492104711ba03b0b
Punnapareddy Bhaskar Rao <bhasaprp2020@gmail.com>
2024-12-21 09:10:32 +0530

    created api route to get all posts

--- backend/controllers/PostControllers.js
+++ backend/controllers/PostControllers.js
@@ -20,6 +20,41 @@ const createPost = async (req, res,next) => {
     }
 }
 
+const getPosts = async (req, res,next) => {
+    try {
+       const startIdx=parseInt(req.query.startIdx) || 0
+         const limit=parseInt(req.query.limit) || 9
+         const sortDirection=req.query.order==='asc'?1:-1
+         const posts=await Post.find({
+            ...(req.query.userId && {userId:req.query.userId}),
+            ...(req.query.slug && {slug:req.query.slug}),
+            ...(req.query.category && {category:req.query.category}),
+            ...(req.query.postId && {_id:req.query.postId}),
+            ...(req.query.searchTerm && {
+                $or:[
+                    {title:{$regex:req.query.searchTerm,$options:'i'}},
+                    {content:{$regex:req.query.searchTerm,$options:'i'}},
+                ]
+            })
+         }).sort({createdAt:sortDirection}).skip(startIdx).limit(limit)
+         const totalPosts=await Post.countDocuments() 
+         const now=new Date()
+         const oneMonthAgo=new Date(
+                now.getFullYear(),
+                now.getMonth()-1,
+                now.getDate(),
+         )
+         const lastMonthPosts=await Post.countDocuments({
+                createdAt:{$gte:oneMonthAgo}
+            })
+
+            return res.status(200).json({posts,totalPosts,lastMonthPosts})
+         
+    } catch (error) {
+        return next(error)
+    }
+}
 module.exports = {
     createPost,
+    getPosts,
 }
--- backend/routes/PostRoutes.js
+++ backend/routes/PostRoutes.js
@@ -1,8 +1,9 @@
 const express=require('express')
 const { verifyUser } = require('../utils/verifyUser')
-const { createPost } = require('../controllers/PostControllers')
+const { createPost, getPosts } = require('../controllers/PostControllers')
 const PostRoutes=express.Router()
 PostRoutes.post('/create',verifyUser,createPost)
+PostRoutes.get('/get',verifyUser,getPosts)
 module.exports={
     PostRoutes,
 }
